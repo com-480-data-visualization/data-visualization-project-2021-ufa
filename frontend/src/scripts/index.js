49904d814bf3df7b5a3a8384f676083b487610ad
Florian Cassayre <florian.cassayre@gmail.com>
2021-05-02 09:27:02 +0200

    Install d3, three and tailwind

--- frontend/src/scripts/index.js
+++ frontend/src/scripts/index.js
@@ -1,4 +1,9 @@
 import '../styles/index.scss';
+import 'tailwindcss/tailwind.css';
+// eslint-disable-next-line no-unused-vars
+import * as d3 from 'd3';
+// eslint-disable-next-line no-unused-vars
+import * as THREE from 'three';
 
 if (process.env.NODE_ENV === 'development') { // Do not remove: used for hot reload
   require('../index.html');
