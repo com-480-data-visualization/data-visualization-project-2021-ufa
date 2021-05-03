4a5f0d3339a2a8c4db24f13e78b1e8ef834a0072
Florian Cassayre <florian.cassayre@gmail.com>
2021-05-03 18:49:04 +0200

    Improve interaction

--- frontend/src/scripts/index.js
+++ frontend/src/scripts/index.js
@@ -53,34 +53,31 @@ const categories = [
 
 const fetchJson = filename => fetch(`public/data/${filename}`).then(response => response.json());
 
-// set the dimensions and margins of the bar plot
+// Set the dimensions and margins of the bar plot
 let margin = { top: 10, right: 30, bottom: 90, left: 40 },
-  width_bar = 360 - margin.left - margin.right,
-  height_bar = 350 - margin.top - margin.bottom;
+  widthBar = 360 - margin.left - margin.right,
+  heightBar = 350 - margin.top - margin.bottom;
 
-//Category Similarity Bar
+// Category Similarity Bar
 
-function bar_plot(dataBar){
-  
-  
+function barPlot(dataBar){
+
   d3.select('#similar-bar').select('svg').remove();
-
-  // append the svg object to the body of the page
 
   let svg_bar = d3.select('#similar-bar')
     .append('svg')
-    .attr('width', width_bar + margin.left + margin.right)
-    .attr('height', height_bar + margin.top + margin.bottom)
+    .attr('width', widthBar + margin.left + margin.right)
+    .attr('height', heightBar + margin.top + margin.bottom)
     .append('g')
     .attr('transform',
       'translate(' + margin.left + ',' + margin.top + ')');
   // X axis
   let x = d3.scaleBand()
-    .range([ 0, width_bar ])
+    .range([ 0, widthBar ])
     .domain(dataBar.map(function(d) { return d.id; }))
     .padding(0.2);
   svg_bar.append('g')
-    .attr('transform', 'translate(0,' + height_bar + ')')
+    .attr('transform', 'translate(0,' + heightBar + ')')
     .call(d3.axisBottom(x))
     .selectAll('text')
     .attr('transform', 'translate(-10,0)rotate(-45)')
@@ -89,7 +86,7 @@ function bar_plot(dataBar){
   // Add Y axis
   let y = d3.scaleLinear()
     .domain([0, 100])
-    .range([ height_bar, 0]);
+    .range([ heightBar, 0]);
   svg_bar.append('g')
     .call(d3.axisLeft(y));
 
@@ -102,15 +99,17 @@ function bar_plot(dataBar){
     .attr('width', x.bandwidth())
     .attr('fill', '#69b3a2')
   // no bar at the beginning thus:
-    .attr('height', ()=> height_bar - y(0)) // always equal to 0
+    .attr('height', ()=> heightBar - y(0)) // always equal to 0
     .attr('y', ()=>  y(0));
+
+  const hundred = 100;
 
   // Animation
   svg_bar.selectAll('rect')
     .transition()
     .duration(800)
-    .attr('y', function(d) { return y(d.weightPercentage); })
-    .attr('height', function(d) { return height_bar - y(d.weightPercentage); })
+    .attr('y', function(d) { return y(d.weightRatio * hundred); })
+    .attr('height', function(d) { return heightBar - y(d.weightRatio * hundred); })
     .delay(function(d,i){return(i*100);});
 
 }
@@ -143,7 +142,7 @@ Promise.all([
   };
 
   const width = 1000;
-  const height = 1000;
+  const height = 800;
 
   const filterEdges = (data, minEdge) => {
     let links = data.links.filter(f => f.weight >= minEdge).map(d => Object.create(d));
@@ -164,12 +163,51 @@ Promise.all([
     .force('charge', d3.forceManyBody().strength(-100))
     .force('center', d3.forceCenter(width / 2, height / 2));
 
+  let selectedNode = null;
+  let hoveredNode = null;
+
+  const updateHighlights = () => {
+    const node = selectedNode || hoveredNode;
+    const connectedSet = new Set();
+    const connectedWeights = [];
+    if (node) {
+      connectedSet.add(node.id);
+      links.forEach(l => {
+        let neighbour = null;
+        if (l.source.id === node.id) {
+          neighbour = l.target.id;
+        } else if (l.target.id === node.id) {
+          neighbour = l.source.id;
+        }
+        if(neighbour) {
+          connectedSet.add(neighbour);
+          connectedWeights.push({ id: neighbour, weight: l.weight });
+        }
+      });
+    }
+    const weightSum = connectedWeights.map(({ weight }) => weight).reduce((a, b) => a + b, 0);
+    const connectedWeightRatios = connectedWeights
+      .map(obj => ({ ...obj, weightRatio: obj.weight / weightSum }))
+      .sort((x, y) => d3.descending(x.weightRatio, y.weightRatio));
+
+    gNodes.classed('opacity-10', node ? d => !connectedSet.has(d.id) : false);
+    gLinks.classed('opacity-25', node ? d => d.source.id !== node.id && d.target.id !== node.id : false);
+    d3.select('#categories-selected-category').text(node ? node.id : '-');
+    d3.select('#categories-selected-field').text(node ? getCategoryIndexAndLabel(node.id).label : '-');
+    d3.select('#categories-selected-count').text(node ? categoriesCounts[node.id].toLocaleString() : '-');
+
+    barPlot(connectedWeightRatios);
+  };
+
   const drag = simulation => {
 
     function dragstarted(event) {
       if (!event.active) simulation.alphaTarget(0.3).restart();
       event.subject.fx = event.subject.x;
       event.subject.fy = event.subject.y;
+
+      selectedNode = event.subject;
+      updateHighlights();
     }
 
     function dragged(event) {
@@ -181,6 +219,9 @@ Promise.all([
       if (!event.active) simulation.alphaTarget(0);
       event.subject.fx = null;
       event.subject.fy = null;
+
+      selectedNode = null;
+      updateHighlights();
     }
 
     return d3.drag()
@@ -193,7 +234,7 @@ Promise.all([
     //.attr('width', width).attr('height', height)
     .attr('viewBox', [0, 0, width, height]);
 
-  const clusters = svg.append('g')
+  const gClusters = svg.append('g')
     .attr('font-weight', 'bold')
     .style('cursor', 'default')
     .style('user-select', 'none')
@@ -204,26 +245,29 @@ Promise.all([
     .attr('fill', d => categoriesColors[d.index])
     .text(d => d.label);
 
-  const link = svg.append('g')
+  const gLinks = svg.append('g')
     .attr('stroke', 'black')
     .attr('stroke-opacity', 0.5)
     .selectAll('line')
     .data(links)
     .join('line')
+    // Don't apply transition on already transparent objects, this is a serious bottleneck!
+    //.attr('class', 'transition-opacity duration-250')
     .attr('stroke-width', d => 0.00025 * d.weight);
 
-  const node = svg.append('g')
+  const gNodes = svg.append('g')
     .attr('stroke', '#fff')
     .attr('stroke-width', 1.5)
     .style('cursor', 'pointer')
     .selectAll('circle')
     .data(nodes)
     .join('circle')
+    .attr('class', 'transition-opacity duration-250')
     .attr('r', d => Math.log(categoriesCounts[d.id]) * 0.7)
     .attr('fill', color)
     .call(drag(simulation));
 
-  node.append('title')
+  gNodes.append('title')
     .text(d => {
       const label = getCategoryIndexAndLabel(d.id).label;
       return `${d.id} (${label})`;
@@ -233,13 +277,13 @@ Promise.all([
     const clampX = x => Math.max(0, Math.min(width, x));
     const clampY = y => Math.max(0, Math.min(height, y));
 
-    link
+    gLinks
       .attr('x1', d => clampX(d.source.x))
       .attr('y1', d => clampY(d.source.y))
       .attr('x2', d => clampX(d.target.x))
       .attr('y2', d => clampY(d.target.y));
 
-    node
+    gNodes
       .attr('cx', d => clampX(Math.max(0, Math.min(width, d.x))))
       .attr('cy', d => clampY(Math.max(0, Math.min(height, d.y))));
 
@@ -257,43 +301,29 @@ Promise.all([
       return { x: xs / nodes.length, y: ys / nodes.length - 10 };
     };
     const aggregated = Object.fromEntries(Object.entries(groups).map(([index, nodesIn]) => [index, mean(nodesIn)]));
-    clusters
+    gClusters
       .attr('x', d => aggregated[d.index].x)
       .attr('y', d => aggregated[d.index].y);
   });
 
-  node.on('mouseover', function (_, d) {
-    const thisNode = d.id;
-    const connected = links.filter(function (e) {
-      return e.source.id === thisNode || e.target.id === thisNode;
-    });
-
-    node.attr('opacity', function (d) {
-      return (connected.map(d => d.source.id).indexOf(d.id) > -1 || connected.map(d => d.target.id).indexOf(d.id) > -1) ? 1 : 0.1;
-    });
-
-    link.attr('opacity', function (d) {
-      return (d.source.id === thisNode || d.target.id === thisNode) ? 1 : 0.1;
-    });
-    let weightSum = d3.sum(connected,d=>d.weight);
-    let neighbourData = connected.map(e =>{
-      let neighbourNode = '';
-      if (e.source.id === thisNode){
-        neighbourNode = e.target.id;
-      } else {
-        neighbourNode = e.source.id;
+  gNodes
+    .on('mouseover', (_, d) => {
+      hoveredNode = d;
+      if(!selectedNode) { // Avoid expensive updates
+        updateHighlights();
       }
-      return { id:neighbourNode,weightPercentage:e.weight*100/weightSum };
-    }).sort(function(x, y){
-      return d3.descending(x.weightPercentage, y.weightPercentage);
-    });
-    
-    bar_plot(neighbourData);
-  })
+    })
     .on('mouseout', () => {
-      node.attr('opacity', 1);
-      link.attr('opacity', 1);
+      hoveredNode = null;
+      if(!selectedNode) {
+        updateHighlights();
+      }
     });
+  // Disabled for now, future use case
+  /*.on('click', (_, d) => {
+      selectedNode = d;
+      updateHighlights();
+    });*/
 
   d3.select('#categories-graph').node().append(svg.node());
   //invalidation.then(() => simulation.stop());
